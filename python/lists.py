9132ec0b43e47789621635e647bb27f430349a80
Abolfazl Mohammadi <afmmyam@gmail.com>
2022-04-14 18:08:46 +0430

    Update lists.py

--- python/lists.py
+++ python/lists.py
@@ -1,3 +1,6 @@
+#Here, I have created a dictionary by combining elements of two lists; one set of 
+# elements as keys and the other as values. #Python 
+
 list_1 =[ 'name', 'age', 'city', 'country']
 list_2 = ['John', '25', 'Tehran', 'Iran ']
 dict_1={list_1[i]:list_2[i] for i in range(len(list_1))}
