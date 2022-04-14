9a71232db989bdba9ef1143ff73b8bad9bbd3fb8
Abolfazl Mohammadi <afmmyam@gmail.com>
2022-04-14 18:02:38 +0430

    Update lists.py

--- python/lists.py
+++ python/lists.py
@@ -1,4 +1,4 @@
 list_1 =[ 'name', 'age', 'city', 'country']
-list_2 = ['John', '25', 'Tehran', 'Iran']
+list_2 = ['John', '25', 'Tehran', 'Iran ']
 dict_1={list_1[i]:list_2[i] for i in range(len(list_1))}
 print(dict_1)
