36013fee9cda74bd076a4a0483695e85dd804be3
abolfazlmim <afmmyam@gmail.com>
2022-05-23 21:36:44 +0200

    Create urllibex.py

---
+++ urllibex.py
@@ -0,0 +1,8 @@
+import urllib.request
+value = input("Please enter a Website Adress with https/http:\n")
+url=urllib.request.urlopen(value)
+print(url.geturl())
+print(url.info())
+header = url.info()
+print(header.as_string())
+print(url.getcod())
